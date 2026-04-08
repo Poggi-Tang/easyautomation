feda5049e4958a4159e7ba8c2a98860d014008aa
Poggi-Tang <97998989+Poggi-Tang@users.noreply.github.com>
2026-04-08 20:19:37 +0800

    release v0.1.3

--- src/easy_uiauto/__init__.py
+++ src/easy_uiauto/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """easy-uiauto - UI automation toolkit based on pyautogui and uiautomation"""
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Poggi-Tang"
 
 from .ctrl import (
